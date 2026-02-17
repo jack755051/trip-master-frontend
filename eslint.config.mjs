3256e33ad1b2246abd38e5cef49bea4cf0bf79e6
charlieTai <charlie.tai@tpisoftware.com>
2026-02-18 05:44:00 +0800

    add : 完成排版設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier"; // 1. 引入 Prettier 配置
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  // 2. 將 Prettier 配置放在最後，它會覆蓋並關閉前面可能衝突的格式規則
+  eslintConfigPrettier,
+
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
